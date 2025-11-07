16828250b340f75b716eee48cab7b3291e9ca49a
agrahafiz13 <agrahafiz2@gmail.com>
2025-11-07 23:48:41 +0700

    menambahkan teks dan wrapping ke centre

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -7,14 +7,33 @@ class ScreenLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: const Column(
-            children: [
-              SizedBox(height: 10),
-              Icon(Icons.lock_outline, size: 80, color: Colors.pink),
-              SizedBox(height: 10),
-            ],
+        child: Center(
+          // ⬅️ Tambahkan Center di sini
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 50),
+                Icon(Icons.lock_outline, size: 80, color: Colors.pink),
+                SizedBox(height: 20),
+                Text(
+                  "Welcome",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Silakan login untuk melanjutkan",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+              ],
+            ),
           ),
         ),
       ),
