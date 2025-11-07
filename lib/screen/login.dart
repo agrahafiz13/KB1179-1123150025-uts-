0491d6c596a76a6a6b091aa4f3798070b00749ba
agrahafiz13 <agrahafiz2@gmail.com>
2025-11-07 23:41:17 +0700

    menambahkan icon gembok ke login screen

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,11 +6,16 @@ class ScreenLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: const Center(
-        child: Text(
-          "Login",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.lock_outline, size: 80, color: Colors.pink),
+              SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
