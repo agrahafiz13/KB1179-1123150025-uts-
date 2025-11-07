251583afd68e4952be5f99052441327d11f01ccf
agrahafiz13 <agrahafiz2@gmail.com>
2025-11-07 23:59:56 +0700

    menambahkan input field column email

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -8,17 +8,16 @@ class ScreenLogin extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          // ⬅️ Tambahkan Center di sini
           child: Container(
             padding: const EdgeInsets.all(20.0),
-            child: const Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: 50),
-                Icon(Icons.lock_outline, size: 80, color: Colors.pink),
-                SizedBox(height: 20),
-                Text(
+                const SizedBox(height: 50),
+                const Icon(Icons.lock_outline, size: 80, color: Colors.pink),
+                const SizedBox(height: 20),
+                const Text(
                   "Welcome",
                   style: TextStyle(
                     fontSize: 28,
@@ -26,12 +25,36 @@ class ScreenLogin extends StatelessWidget {
                     color: Colors.black,
                   ),
                 ),
-                SizedBox(height: 8),
-                Text(
+                const SizedBox(height: 8),
+                const Text(
                   "Silakan login untuk melanjutkan",
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                  style: TextStyle(fontSize: 16, color: Colors.pink),
                 ),
+                const SizedBox(height: 30),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Masukkan email Anda',
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: Colors.pink[700]!,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
               ],
             ),
           ),
