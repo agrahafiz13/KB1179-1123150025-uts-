3c5afb0ea119f8b44d40c360a935879eb6fc35b4
agrahafiz13 <agrahafiz2@gmail.com>
2025-11-07 20:32:25 +0700

    Membuat image bulat di tengah

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -6,18 +6,21 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.pink,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
         //Todo:
         // karena akan membuat multiple widget
         // maka membuat children yang bisa menampung banyak
