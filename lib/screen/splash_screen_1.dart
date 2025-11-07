48815a04f4d4a725a0dad7f24a719a0febdfa4ae
agrahafiz13 <agrahafiz2@gmail.com>
2025-11-07 20:28:07 +0700

    Membuat Image bulat didalam column

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -5,6 +5,30 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.pink,
+            ),
+          ),
+        ],
+        //Todo:
+        // karena akan membuat multiple widget
+        // maka membuat children yang bisa menampung banyak
+        // widget "children"
+        // membuat Image bulat
+        // Membuat Title dengan warna hitam
+        // Membuat subtitle
+        // Membuat 3 bullet kecil
+        //. mulai dari warna terang diawal
+        // Membuat Button Continue
+      ),
+    );
   }
 }
